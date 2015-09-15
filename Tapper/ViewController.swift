707dedeb268f1c9e2c6c071301b72aa5fc2cdcbb
Daniel Zhang <Daniel@Daniels-MacBook-Pro.local>
2015-09-15 14:09:41 -0500

    finished tapper app

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -8,18 +8,88 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //Properties
+    
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    
+    
+    //Outlets
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLabel: UILabel!
+    
+    @IBAction func onCoinTapped(sender: UIButton!){
+        currentTaps++
+        updateTapsLabel()
+        
+        if isGameOver(){
+            restartGame()
+            
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func playButtonPressed(sender: UIButton!){
+       
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            
+            logoImage.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            tapBtn.hidden = false
+            tapsLabel.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTapsLabel()
+            
+        
+            
+            }
+        }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+            howManyTapsTxt.resignFirstResponder()
+        return true
     }
+    
+    
+    func restartGame(){
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        logoImage.hidden = false
+        playBtn.hidden = false
+        howManyTapsTxt.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLabel.hidden = true
+    }
+    
+    func isGameOver() -> Bool{
+        if currentTaps >= maxTaps{
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    func updateTapsLabel(){
+        tapsLabel.text = "\(currentTaps) Taps"
 
-
+        
+        
+    }
+    
 }
 
